c387c5c326797b7af0a0a462b5f968889d5286c1
Yaa <antwiyaa17@gmail.com>
2026-03-08 18:52:21 +0000

    Resolving the color issue

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { menuItems } from "../utils/menuItems";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between ml-13 mt-7 p-3 bg-gray-dark rounded-3xl w-[1153.78px] h-[52]">
+    <nav className="flex items-center justify-between ml-13 mt-7 p-3 bg-gray-700 rounded-3xl w-[1153.78px] h-[52]">
      <div className="flex items-center ">
         <ul className="flex justify-around gap-5">
         {menuItems.map(menu =>
@@ -14,9 +14,9 @@ const Navbar = () => {
         )}
         </ul>
      </div>
-
       <button className="flex text-sm bg-gray-800 text-white px-4 py-2 rounded-md ml-4">Resume</button>
     </nav>
+    
   );
 }
 
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,30 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
 
-  darkMode: 'class',
   
+
   theme: {
     extend: {
       screens: {
-        md: '768px',
+        md: "768px",
       },
       colors: {
-        primary: '#5483B3',
-        secondary: '#3B82F6',
-        sec: '#052659',
+        primary: "#5483B3",
+        secondary: "#3B82F6",
+        sec: "#052659",
         gray: {
-        darkest: '#171717',
-        dark: '#344054',
-        DEFAULT: '#c0ccda',
-        light: '#e0e6ed',
-        lightest: '#f9fafc',
-      }
-      }
-    }
-  }
+          darkest: "#171717",
+          dark: "#344054",
+          DEFAULT: "#c0ccda",
+          light: "#e0e6ed",
+          lightest: "#f9fafc",
+        },
+      },
+    },
+  },
+
+  plugins: [],
 } satisfies Config
