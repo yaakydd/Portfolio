de3e359a40bacf155f3301743fde89609bc85b6e
Yaa <antwiyaa17@gmail.com>
2026-03-09 05:08:20 +0000

    Changing the hero page design

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,7 +1,24 @@
 import Navbar from "../components/Navbar";
 const LandingPage = () => {
+
     return (
+        <>
         <Navbar />
+<div className="relative flex flex-col items-center justify-end w-full h-125 overflow-hidden">
+      
+      {/* 1. The Blue Semi-Circle */}
+      {/* We use w-[600px] and h-[300px] with rounded-t-full to create the curve */}
+      <div className="absolute bottom-0 w-150 h-75 bg-[#5483B3] rounded-t-full z-0"></div>
+
+      {/* 2. The Image (Layered on top) */}
+      <img 
+        src="/yaa-portrait.png" 
+        alt="Yaa, Data Scientist" 
+        className="relative z-10 h-112.5 object-contain" 
+      />
+    </div>
+        </>
+        
 
     );
 }
