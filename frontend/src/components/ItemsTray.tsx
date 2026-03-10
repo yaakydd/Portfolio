5dc9f32069cba191b1dc263492b8cf6ddce46e56
Yaa <antwiyaa17@gmail.com>
2026-03-10 07:43:20 +0000

    Applying hover effect to the items tray

--- frontend/src/components/ItemsTray.tsx
+++ frontend/src/components/ItemsTray.tsx
@@ -1,8 +1,8 @@
 const ItemsTray = () => {
 
     return (
-        <div className="py-4">
-            <ul className="items-center justify-center gap-7 font-jakarta text-sm flex">
+        <div className="py-2">
+            <ul className="flex items-center justify-center gap-7 font-jakarta text-sm hover:text-white hover:bg-amber-600 rounded-2xl">
                 <li><a href="#">All posts</a></li>
                 <li><a href="#">Data Science</a></li>
                 <li><a href="#">Machine Learning</a></li>
