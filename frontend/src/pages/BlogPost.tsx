878094cd2d7b1ba97999065e805e7905ed9419a5
Yaa <antwiyaa17@gmail.com>
2026-03-13 07:30:45 +0000

    Applied the background color to the first p element in the page

--- frontend/src/pages/BlogPost.tsx
+++ frontend/src/pages/BlogPost.tsx
@@ -3,8 +3,8 @@ import ItemsTray from "../components/ItemsTray";
 
 const blogItems = [
   { name: "All Posts", link: "#" },
-  { name: "AI", link: "#" },
-  { name: "React", link: "#" },
+  { name: "Data Science", link: "#" },
+  { name: "Machine Learning", link: "#" },
   { name: "Tech News", link: "#" },
 ];
 
@@ -14,7 +14,7 @@ const BlogPost = () => {
         <>
         <Navbar />
         <div className="px-5 py-7 overflow-x-hidden">
-            <p className="text-center mt-2 text-[#3b82f6] text-sm">Insights & Ideas</p>
+            <p className="relative left-[45%] text-center mt-2 text-[#3b82f6] rounded-4xl w-35 text-sm px-1 py-1 bg-[#3b82f6]/25">Insights & Ideas</p>
             <h1 className="text-5xl font-jakarta py-6 text-center font-extrabold text-[#1e293b]">From my <span className="text-[#3b82f6]">blog post</span></h1>
             <p className="text-center text-md font-light text-[#64748b]">Explore the intersection of data science, machine learning and modern <br />
             technology. Dive into my latest thoughts and discoveries. </p>
