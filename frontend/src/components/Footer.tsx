74017a058e2057f4c2023ffe744f4e763124df1b
Yaa <antwiyaa17@gmail.com>
2026-04-14 11:29:03 +0000

    Almost done with the right side

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,31 +2,29 @@ import { ArrowUpRight } from "lucide-react"
 const Footer = () => {
     return(
         <footer className="bg-[#272727] rounded-tl-[30px] rounded-tr-[30px] w-330 ml-6 h-100 text-white py-2 text-center">
-            <div className="flex justify-between py-3">
-                <h1 className="text-5xl font-bold text-left ml-7 py-5 ">Let's <span className="text-[#3B82F6]">Connect</span></h1>
+            <div className="flex justify-between py-4">
+                <h1 className="text-5xl font-bold text-left ml-10 py-3 ">Let's <span className="text-[#3B82F6]">Connect</span></h1>
                 <button className="bg-[#5483B3] mr-16 w-33 h-10 mt-7 flex justify-center items-center text-lg rounded-3xl cursor-pointer">Hire Me<ArrowUpRight size={20} strokeWidth={1.5} className="ml-1" /></button>
             </div>
-            <hr className="border-white w-310 ml-10" />
-            <div className="mt-2 py-10 flex items-center justify-center gap-25">
-                <div className="leading-8">
+            <hr className="border-white w-290 ml-21" />
+            <div className="mt-2 py-8 flex items-center justify-center gap-27">
+                <div className="leading-8 inline-block">
                     <h1 className="text-left font-bold text-[#5483B3]"><i className="text-[17px]">Great Things Begin Very Little</i></h1>
                     <p className="font-semibold text-[17px]">Have an awesome project idea, let's discuss. <br /> 
                     <span className="text-purple-500 mr-2 font-normal text-[17px]">"Fun Fact" : Data tells a story, I help it speak clearly</span> 
-                    
                     </p>
-
                 </div>
-                <div className="text-left leading-8">
-                    <h1 className="text-[17px] text-[#5483B3] font-bold">Contact</h1>
+                <div className="text-left leading-10">
+                    <h1 className="text-[20px] text-[#5483B3] font-bold">Contact</h1>
                     <p>0546 345 107</p>
                     <a href="mailto:antwiyaa17@gmail.com">antwiyaa17@gmail.com</a>
                 </div>
-                <div>
-                    <h1></h1>
-                    <p>Have an awesome project idea, let's discuss</p>
+                <div className="block text-left mb-11 ">
+                    <h1 className="text-[#5483B3] text-[20px] font-bold  ">Get the latest information</h1>
+                    <input type="email" placeholder="Email Address"  className="mt-3 rounded-tl-[12.44px] text-black  bg-white border-white rounded-bl-[12.44px] h-10" /> <hr />
                 </div>
             </div>
-            <hr className="border-white w-310 ml-10"  />
+            <hr className="border-white w-290 ml-21 "  />
         </footer>
     )
 }
